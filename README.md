dacb9d695cb9e3f44b72b528b9e27699a6b0b2c5
Michael Stawowy <pytton@hotmail.com>
2015-10-27 00:58:30 -0600

    Initial commit. Clean this code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # FirstIBAPI
 First IB API code that worked without thread
+
+This code was modified from the one originally delivered by Abhijit by removing threads. It works. It connects to Interactive Brokers TWS and pulls bid/ask values for SPY.
+
+IN ORDER TO WORK, THE 'ALLOW CONNECTIONS FROM LOCALHOST ONLY' option has to be TICKED in Interactive Brokers TWS Software. 
+
+This code uses Jan Boonen's Libraries located here: https://github.com/JanBoonen/TwsApiCpp
+
+This has to be cleaned - find the location of source files, change names, create new solution.
+
+Currently, solution located here:
+
+C:\...\FirstIBAPI\TwsApiCpp-master_New\TwsApiC++\_win32
+
+
---
+++ TwsApiCpp-master_New/TwsApiC++/Test/_win32/APINoThread.cpp
@@ -0,0 +1,143 @@
+#include <conio.h>
+#include <iostream>
+#include <iomanip> //for setprecision
+
+#include <Windows.h>
+
+#include "TwsApiL0.h"
+#include "TwsApiDefs.h"
+//#include <process.h>
+using namespace TwsApi;
+
+int generateID()
+{
+	static int s_itemID = 0;
+	return s_itemID++;
+}
+
+void waitforuserend()
+{
+	using namespace std;
+	cin.clear();
+	cin.ignore(255, '\n');
+	cin.get();
+}
+
+
+class ResponseFromTWS : public EWrapperL0
+{
+public:
+
+	double m_priceAsk;
+	double m_priceBid;
+	double m_BidSize;
+	double m_AskSize;
+
+	virtual void tickPrice(TickerId tickerId, TickType field, double price, int canAutoExecute)
+	{
+		switch (field)
+		{
+		case TickType::ASK:
+			m_priceAsk = price;
+			break;
+		case TickType::BID:
+			m_priceBid = price;
+			break;
+		}
+	}
+
+	virtual void tickSize(TickerId tickerId, TickType field, int size)
+	{
+		switch (field)
+		{
+		case TickType::BID_SIZE:
+			m_BidSize = double(size);
+			break;
+		case TickType::ASK_SIZE:
+			m_AskSize = double(size);
+			break;
+
+		}
+	}
+};
+
+void GetContractDetails(Contract & spyContract)
+{
+
+	// use either set to get what you want. both work
+	//forex (GBP) works 24/7 mon-friday
+
+	spyContract.symbol = "GBP";
+	spyContract.secType = *SecType::CASH;		//"STK"
+	spyContract.currency = "USD";
+	spyContract.exchange = *Exchange::IDEALPRO;	//"SMART";
+
+
+	//spyContract.symbol	= "SPY";
+	//spyContract.secType	= *SecType::STK;		//"STK"
+	//spyContract.currency	= "USD";
+	//spyContract.exchange	= *Exchange::ARCA;	//"SMART";
+	//spyContract.primaryExchange = *Exchange::ARCA;	//"ARCA";
+}
+
+int StartTWSConn()
+{
+	//create a function that connects to TWS
+	//create a class that has the following:
+	//setter - creates a new random number
+	//getter - returns a random float
+
+	//create a for loop that iterates 5 times
+	//call the function to get a random number
+	//cast the number to char *
+	//print the number
+
+
+	//set up the first thread:
+	ResponseFromTWS	 * twsReply = new ResponseFromTWS();
+	EClientL0*	pClientTWS = EClientL0::New(twsReply);
+	bool bIsConnected = pClientTWS->eConnect("", 7496);
+	//the below works without launching it in a thread:
+	Contract	spyContract;
+	GetContractDetails(spyContract);
+	int  tickerID = 100;
+	pClientTWS->reqMktData(tickerID, spyContract, "", false);
+
+
+
+	using namespace std;
+
+	for (int i = 0; i < 20; i++)
+	{
+		int Wait = 250;
+		Sleep(Wait);
+		cout << "sleeping for: " << Wait << " ms" << endl;
+
+		pClientTWS->reqMktData(tickerID, spyContract, "", false);
+		double ask = twsReply->m_priceAsk;
+		double bid = twsReply->m_priceBid;
+		double bidSize = twsReply->m_BidSize;
+		double askSize = twsReply->m_AskSize;
+		
+		//long currentTime = twsReply->currentTime(); //not working
+		cout << setprecision(15);
+
+		cout << "ask: " << ask << endl;
+		cout << "bid: " << bid << endl;
+		cout << "ask size: " << askSize << endl;
+		cout << "bid size: " << bidSize << endl;
+
+		//cout << "time: " << currentTime << endl;
+	}
+
+
+	cout << endl << "Finished." << endl;
+	waitforuserend();
+
+	////////////////////////////////////////////////////////////////
+	pClientTWS->eDisconnect();
+	delete twsReply;
+	////////////////////////////////////////////////////////////////
+
+	return 0;
+}
